193d3f817bb12ada5c7dc19de47204b7b07aa2ad
sumeetby16 <sumeetby16>
2026-01-08 16:49:37 +0530

    lib

--- cie3/test_lib.py
+++ cie3/test_lib.py
@@ -1,10 +1,11 @@
+
 from library import bookdetails
 
 def test_lib():
-    op=(
-        f"book id:id\n"
-        f"book title:title\n"
-        f"author name:name\n"
-        f"Year of publication:publication\n"
-        )
-    assert bookdetails (1001,"jurasic park","veeresh",1999) == op
+    op = (
+        "book id:1001\n"
+        "book title:jurasic park\n"
+        "author name:veeresh\n"
+        "Year of publication:1999\n"
+    )
+    assert bookdetails(1001, "jurasic park", "veeresh", 1999) == op
